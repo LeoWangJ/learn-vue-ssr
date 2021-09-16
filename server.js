0660d9cacb19d72c1547e4de767184c20bc9c96d
leowang <leowang@jetfuel.com.tw>
2021-09-16 16:04:15 +0800

    ✨ 重構 vue 實例

---
+++ server.js
@@ -0,0 +1,34 @@
+const createApp = require('./app')
+
+const server = require('express')();
+
+const template = require('fs').readFileSync('./index.template.html', 'utf-8');
+
+const renderer = require('vue-server-renderer').createRenderer({
+  template,
+});
+
+const context = {
+    title: 'vue ssr',
+    meta: `
+        <meta name="keyword" content="vue,ssr">
+        <meta name="description" content="vue srr demo">
+    `,
+};
+server.get('*', (req, res) => {
+    const data = { url: req.url }
+    const app = createApp(data)
+
+    renderer
+    .renderToString(app,context).then(html=>{
+        res.end(html);
+    }).catch(err=>{
+        console.log(err)
+        res.status(500).end('Internal Server Error')
+    });
+  })
+  
+
+server.listen(5000,()=>{
+    console.log('http://localhost:5000')
+})
